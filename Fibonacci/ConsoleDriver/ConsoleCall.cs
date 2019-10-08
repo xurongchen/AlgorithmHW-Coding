e60b42b235bef60a644502d651dc46a74d8d629d
xurongchen <1459572152@qq.com>
2019-10-08 18:49:51 +0800

    Fibonacci save

--- Fibonacci/ConsoleDriver/ConsoleCall.cs
+++ Fibonacci/ConsoleDriver/ConsoleCall.cs
@@ -1,9 +1,24 @@
 ﻿using System;
+using System.IO;
+using System.Numerics;
+using System.Threading;
 
 namespace ConsoleDriver {
     class ConsoleCall {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            var sw = new StreamWriter(Console.OpenStandardOutput()) {AutoFlush = true};
+            Console.SetOut(sw);
+            Console.WriteLine("Input a number:");
+            var numStr = Console.ReadLine();
+            BigInteger num;
+            if (BigInteger.TryParse(numStr,out num)) {
+                var experimentE =
+                    new Experiment(new FibonacciExecutor(new Core.FibonacciEfficient(), num),logStreamWriter: sw);
+                experimentE.Start();
+                while(experimentE.Now == Experiment.State.Running) Thread.Sleep(500);
+                return;
+            }
+            Console.WriteLine("[Error] Please input a number.");
         }
     }
 }
